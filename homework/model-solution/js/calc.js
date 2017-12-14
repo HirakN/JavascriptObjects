646fc5b231f16c71f0be6aabfd70bb3176ab4290
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2017-12-14 12:27:59 +0000

    added event listener for equals and added calculate function

--- homework/model-solution/js/calc.js
+++ homework/model-solution/js/calc.js
@@ -1,38 +1,56 @@
 // Add a click event listener to a button DONE
 // Add click event listeners for each button * DONE
 // Event listener accesses the text from the button that is clicked DONE
+// Enter the text from the number and operator buttons onto the screen in the HTML *
 
 var numberButtons = document.getElementsByClassName('buttonNum');
 var operatorButtons = document.getElementsByClassName('operator');
 var display = document.getElementById('screen');
+var equalsButton = document.getElementsByClassName('equals')[0];
+
 function addButtonListeners() {
 	for (var ii = 0; ii < numberButtons.length; ii++) {
 		var button = numberButtons[ii];
 		button.addEventListener("click", function(event) {
-			console.log(event.target.innerHTML);
+			displayText(event.target.innerHTML);
 		});
 	}
 
 	for (var ii = 0; ii < operatorButtons.length; ii++) {
 		var button = operatorButtons[ii];
 		button.addEventListener("click", function() {
-			console.log(this.innerHTML);
+			displayText(this.innerHTML);
 		});
 	}
+
+	equalsButton.addEventListener("click", function() {
+		calculate();
+	})
 }
 
 addButtonListeners();
 
-// Enter the text from the number and operator buttons onto the screen in the HTML *
 function displayText(text) {
 	return display.innerHTML = text;
 }
 
-displayText("hello");
 // Add event listener to the calculate button that fires function called "calculate" *
 
 // Write the calculate function to take two numbers and an operator and return the answer
-
+function calculate(firstNum, secondNum, operator) {
+	switch(operator) {
+		case "+":
+			return firstNum + secondNum;
+		case "-";
+			return firstNum - secondNum;
+		case "*":
+			return firstNum * secondNum;
+		case "/":
+			return firstNum / secondNum;
+		default:
+			return throw "Incorrect Operator";
+	}
+}
 // Display the answer on the screen
 
 // Add an event listener to the rest button to clear the page and the variables *
